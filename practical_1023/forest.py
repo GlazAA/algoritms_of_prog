badde6187b2591bd153a8b1c4c45b7c7c1de2151
GlazAA <113853477+GlazAA@users.noreply.github.com>
2024-10-23 14:27:04 +0300

    Add files via upload

---
+++ practical_1023/forest.py
@@ -0,0 +1,114 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if not self.root:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert_recursive(node.right, value)
+
+    def search(self, value):
+        return self._search_recursive(self.root, value)
+
+    def _search_recursive(self, node, value):
+        if node is None or node.value == value:
+            return node is not None
+        if value < node.value:
+            return self._search_recursive(node.left, value)
+        return self._search_recursive(node.right, value)
+
+    def delete(self, value):
+        self.root = self._delete_recursive(self.root, value)
+
+    def _delete_recursive(self, node, value):
+        if node is None:
+            return None
+        if value < node.value:
+            node.left = self._delete_recursive(node.left, value)
+        elif value > node.value:
+            node.right = self._delete_recursive(node.right, value)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            min_node = self._find_min(node.right)
+            node.value = min_node.value
+            node.right = self._delete_recursive(node.right, min_node.value)
+        return node
+
+    def _find_min(self, node):
+        current = node
+        while current.left:
+            current = current.left
+        return current
+
+    def in_order_traversal(self):
+        result = []
+        self._in_order_recursive(self.root, result)
+        return result
+
+    def _in_order_recursive(self, node, result):
+        if node:
+            self._in_order_recursive(node.left, result)
+            result.append(node.value)
+            self._in_order_recursive(node.right, result)
+
+    def pre_order_traversal(self):
+        result = []
+        self._pre_order_recursive(self.root, result)
+        return result
+
+    def _pre_order_recursive(self, node, result):
+        if node:
+            result.append(node.value)
+            self._pre_order_recursive(node.left, result)
+            self._pre_order_recursive(node.right, result)
+
+    def post_order_traversal(self):
+        result = []
+        self._post_order_recursive(self.root, result)
+        return result
+
+    def _post_order_recursive(self, node, result):
+        if node:
+            self._post_order_recursive(node.left, result)
+            self._post_order_recursive(node.right, result)
+            result.append(node.value)
+
+# тест
+bst = BinarySearchTree()
+
+values = [50, 800, 70, 20, 400000, 60, 80]
+for value in values:
+    bst.insert(value)
+
+print("Поиск 50:", bst.search(50))
+print("Поиск 99:", bst.search(99))
+
+# удаляем
+bst.delete(20)
+bst.delete(60)
+
+print("In-order обход:", bst.in_order_traversal())
+print("Pre-order обход:", bst.pre_order_traversal())
+print("Post-order обход:", bst.post_order_traversal())
